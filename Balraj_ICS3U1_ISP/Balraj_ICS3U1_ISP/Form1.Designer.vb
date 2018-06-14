68f24d2f9224f348554dc832a26cbb15dcf8bfef
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-14 15:01:48 -0400

    Add files via upload

---
+++ Balraj_ICS3U1_ISP/Balraj_ICS3U1_ISP/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReverse = New System.Windows.Forms.Button()
+        Me.lblReverse = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnReverse
+        '
+        Me.btnReverse.Location = New System.Drawing.Point(98, 213)
+        Me.btnReverse.Name = "btnReverse"
+        Me.btnReverse.Size = New System.Drawing.Size(123, 39)
+        Me.btnReverse.TabIndex = 0
+        Me.btnReverse.Text = "Reverse"
+        Me.btnReverse.UseVisualStyleBackColor = True
+        '
+        'lblReverse
+        '
+        Me.lblReverse.AutoSize = True
+        Me.lblReverse.Location = New System.Drawing.Point(95, 167)
+        Me.lblReverse.Name = "lblReverse"
+        Me.lblReverse.Size = New System.Drawing.Size(0, 13)
+        Me.lblReverse.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(530, 375)
+        Me.Controls.Add(Me.lblReverse)
+        Me.Controls.Add(Me.btnReverse)
+        Me.Name = "Form1"
+        Me.Text = "ISP"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnReverse As System.Windows.Forms.Button
+    Friend WithEvents lblReverse As System.Windows.Forms.Label
+
+End Class
---
+++ Balraj_ICS3U1_ISP/Balraj_ICS3U1_ISP/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Dim revText, sentence, try1 As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim words, sentence As String
+
+        Do
+            words = InputBox("Enter each word seprately including period:") 'getting the seperate input from the user including the period 
+            sentence = sentence & " " & words 'Accumuilating/concatenating users input in sentence 
+            revText = StrReverse(sentence) 'Using StrReverse function in Visual Basic to reverse the string strored in sentence
+            
+        Loop Until words = "." 'Using period to stop looping through and asking for input from user.
+
+    End Sub
+
+    Private Sub btnReverse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReverse.Click
+
+        lblReverse.Text = revText 'Assigned revText to label to output on the display
+    End Sub
+End Class
