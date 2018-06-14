6a6865855ec692679b1b0ca1404678dadfb4388a
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-14 15:00:55 -0400

    Add files via upload

---
+++ Alec Lin/Encryption Program-1/Encryption Program-1/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MessageBox = New System.Windows.Forms.TextBox()
+        Me.Encrypt = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FinalOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter your message:"
+        '
+        'MessageBox
+        '
+        Me.MessageBox.Location = New System.Drawing.Point(121, 6)
+        Me.MessageBox.Name = "MessageBox"
+        Me.MessageBox.Size = New System.Drawing.Size(151, 20)
+        Me.MessageBox.TabIndex = 1
+        '
+        'Encrypt
+        '
+        Me.Encrypt.Location = New System.Drawing.Point(105, 42)
+        Me.Encrypt.Name = "Encrypt"
+        Me.Encrypt.Size = New System.Drawing.Size(75, 23)
+        Me.Encrypt.TabIndex = 2
+        Me.Encrypt.Text = "Encrypt"
+        Me.Encrypt.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Your encrypted text is:"
+        '
+        'FinalOutput
+        '
+        Me.FinalOutput.AutoSize = True
+        Me.FinalOutput.Location = New System.Drawing.Point(12, 110)
+        Me.FinalOutput.Name = "FinalOutput"
+        Me.FinalOutput.Size = New System.Drawing.Size(0, 13)
+        Me.FinalOutput.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 136)
+        Me.Controls.Add(Me.FinalOutput)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Encrypt)
+        Me.Controls.Add(Me.MessageBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Encrypter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents MessageBox As System.Windows.Forms.TextBox
+    Friend WithEvents Encrypt As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents FinalOutput As System.Windows.Forms.Label
+
+End Class
---
+++ Alec Lin/Encryption Program-1/Encryption Program-1/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Encrypt.Click
+        Dim Message As String = MessageBox.Text 'Declare message as user inputted message
+        Dim EncryptValue As Integer
+        Randomize()
+        EncryptValue = (8) * Rnd() + 1 'Randomly selects how much to increase the unicode by (from 1 to 9)
+        Dim EncryptMessage As String
+        FinalOutput.Text = EncryptValue
+        For i = 1 To Message.Length ' Runs as many times as there are characters in the message
+            EncryptMessage = Asc(Message) ' Converts the firtst character in the string into unicode
+            FinalOutput.Text = FinalOutput.Text & ChrW(EncryptMessage + EncryptValue) ' Adds the random integer to the unicode, then converts it back to characters, then using an accumulator adds it to a label
+            Message = Message.Remove(0, 1) ' Removes the first character from the original message string
+        Next
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles MessageBox.TextChanged
+        FinalOutput.Text = "" ' Clears the output label whenever the input textbox is changed
+    End Sub
+End Class
