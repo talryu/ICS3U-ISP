660e31b6031158edeebf93458df0e001468bda4e
talryu <40275760+talryu@users.noreply.github.com>
2018-06-14 14:08:41 -0400

    Add files via upload

---
+++ Michael ISP/ISP 3.0/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(307, 81)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Encrypt"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(306, 121)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(62, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Decrypt"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(95, 208)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poplar Std", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(224, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(234, 42)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Encryption Program"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 208)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Message is:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(724, 429)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Name = "Form1"
+        Me.Text = "Encryption"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Michael ISP/ISP 3.0/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Dim message As String = InputBox("Enter Message")
+    Dim firstVal As Integer ' This variable will be used to contain the first value of the conversion process
+    Dim secondVal As String ' This variable will be used to contain the second value of the conversion process
+    Dim reversedMessage As String
+    Dim count As Integer 'used to determine if letter is odd or even in decryption, and used to determine when to skip to the next line for encryption
+    Dim secretMessage As String
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        Label1.Text = ""
+        For i As Integer = 0 To message.Length - 1
+            If i Mod 2 = 0 Then ' checks if the letter is even
+                firstVal = AscW(message.Chars(i)) ' converts letter to unicode
+                firstVal = Convert.ToString(firstVal, 2) ' converts unicode to binary
+                If count > 15 Then 'determines if 20 words have been added, if so skips to the next line
+                    Label1.Text += " " & firstVal & vbCrLf 'updates the label with the next value. A space is put between each letter in a word.
+                    count = 0
+                Else : Label1.Text += " " & firstVal 'updates the label with the next value. A space is put between each letter in a word.
+                    count += 1
+                End If
+            ElseIf i Mod 2 <> 0 Then ' checks if the letter is odd 
+                firstVal = AscW(message.Chars(i)) 'converts letter to unicode
+                secondVal = Convert.ToString(firstVal, 16) ' converts unicode to hexadecimal 
+                If count > 15 Then 'determines if 20 words have been added, if so skips to the next line
+                    Label1.Text += " " & secondVal & vbCrLf 'updates the label with the next value. A space is put between each letter in a word.
+                    count = 0
+                Else : Label1.Text += " " & secondVal 'updates the label with the next value. A space is put between each letter in a word.
+                    count += 1
+                End If
+            End If
+        Next
+        secretMessage = Label1.Text
+    End Sub
+
+    Private Sub RadioButton2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles RadioButton2.Click
+        count = 0
+        Dim newMessage As String = secretMessage.Trim
+        newMessage += " " 'adds a space to the end of the message to make sure that the last letter is decrypted
+        Dim binaryConvert As String 'will hold the binary value
+        Dim spaceIndex As Integer
+        Dim hexConvert As String 'will hold hexadecimal value
+        Dim originalNumber As Integer 'will hold the original unicode value
+        Dim nextLetter As String 'will hold the original letter
+        Label1.Text = Nothing
+        For i As Integer = 0 To newMessage.Length - 1
+            If count = message.Length Then
+                Exit For
+            End If
+            If newMessage.Chars(i) = " " Then 'locates a space, all spaces represent the end of 1 letter
+                If count Mod 2 = 0 And newMessage.Chars(i - 1) <> vbCrLf Then 'checks if the letter is even
+                    binaryConvert = newMessage.Substring(spaceIndex, 8) 'determines when the binary string ends by creating a substring starting from the index of the space and going to 8, since all binary values have length of 7.
+                    binaryConvert = binaryConvert.Trim 'deletes any extra spaces to avoid errors in conversion
+                    originalNumber = Convert.ToInt32(binaryConvert, 2) 'converts binary back to unicode
+                    nextLetter = ChrW(originalNumber) 'converts unicode back to original letter
+                    Label1.Text += nextLetter
+                ElseIf count Mod 2 <> 0 And newMessage.Chars(i - 1) <> vbCrLf Then 'checks if the letter is odd
+                    hexConvert = newMessage.Substring(spaceIndex + 1, 3) 'determines when the hexadecimal string ends by creating a substring starting from the index of the space and going to 3, since all binary values have length of 2.
+                    hexConvert = hexConvert.Trim 'deletes any extra spaces to avoid errors in conversion
+                    originalNumber = (Convert.ToInt32(hexConvert, 16)) 'converts hexadecimal back to unicode
+                    nextLetter = ChrW(originalNumber) 'converts unicode back to original letter
+                    Label1.Text += nextLetter
+                Else
+                    Continue For
+                End If
+                count += 1
+                spaceIndex = i
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton2.CheckedChanged
+
+    End Sub
+End Class
