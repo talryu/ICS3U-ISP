10ed395603caadf0530904edc272edb0c3b106ee
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-15 13:06:17 -0400

    Add files via upload

---
+++ Vincet Zhu ISP/Vincent Zhu ISP/Vincent Zhu ISP/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn.Click
+
+        Dim text, combine, reverse As String
+
+        Do
+            text = InputBox("Enter Text(. to stop)", "Reversing Words") 'encrypt is the user input
+            combine = combine & " " & text 'combining the new text with the old text
+            reverse = StrReverse(combine) 'reversing the text and the order of the text
+            lblDecrypt.Text = reverse 'showing the reversed text with reversed order
+        Loop Until text = "|"
+        ' preventing a infinite loop with a stop | to the loop
+    End Sub
+End Class
---
+++ Vincet Zhu ISP/Vincent Zhu ISP/Vincent Zhu ISP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Vincent Zhu ISP")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Vincent Zhu ISP")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("aa3a1477-45fd-40e8-9283-633a3bb40afe")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
