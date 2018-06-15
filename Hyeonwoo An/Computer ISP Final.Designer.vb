4f4f054d260fc6aeae6549567e05b20d3532649a
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-15 12:43:49 -0400

    Add files via upload

---
+++ Hyeonwoo An/Computer ISP Final.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEncode = New System.Windows.Forms.Button()
+        Me.btnDecode = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.txtEncode = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnEncode
+        '
+        Me.btnEncode.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEncode.Location = New System.Drawing.Point(186, 456)
+        Me.btnEncode.Name = "btnEncode"
+        Me.btnEncode.Size = New System.Drawing.Size(80, 35)
+        Me.btnEncode.TabIndex = 0
+        Me.btnEncode.Text = "Encode"
+        Me.btnEncode.UseVisualStyleBackColor = True
+        '
+        'btnDecode
+        '
+        Me.btnDecode.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDecode.Location = New System.Drawing.Point(641, 456)
+        Me.btnDecode.Name = "btnDecode"
+        Me.btnDecode.Size = New System.Drawing.Size(80, 35)
+        Me.btnDecode.TabIndex = 1
+        Me.btnDecode.Text = "Decode"
+        Me.btnDecode.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(393, 465)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 2
+        '
+        'txtEncode
+        '
+        Me.txtEncode.Location = New System.Drawing.Point(12, 89)
+        Me.txtEncode.Multiline = True
+        Me.txtEncode.Name = "txtEncode"
+        Me.txtEncode.Size = New System.Drawing.Size(386, 305)
+        Me.txtEncode.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(500, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Original Text"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(32, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Encode Text:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(432, 254)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "<=>"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(530, 92)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(0, 13)
+        Me.lblAnswer.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(911, 569)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtEncode)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.btnDecode)
+        Me.Controls.Add(Me.btnEncode)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnEncode As System.Windows.Forms.Button
+    Friend WithEvents btnDecode As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents txtEncode As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+
+End Class
---
+++ Hyeonwoo An/Computer ISP Final.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Function Caesar_Cipher(ByVal text As String, ByVal X As Integer, ByVal encode As Boolean) 'X is how much user want to shift the word, text to classify encode or decode
+        Dim Final As String = " "
+        For Each c As Char In text
+            Dim A As Integer
+            If encode Then
+                A = Asc(c) + X
+
+            Else
+                A = Asc(c) + 26 - X
+
+            End If
+
+            If Char.IsLower(c) Then    'Classifying the Capital letters
+                If A > Asc("z") Then
+                    A -= 26
+                End If
+                Final += Chr(A)
+            ElseIf Char.IsUpper(c) Then
+                If A > Asc("Z") Then
+                    A -= 26
+                End If
+                Final += Chr(A)               'Getting the answer
+            Else
+                Final += c
+            End If
+        Next
+        Return Final
+    End Function
+
+    Private Sub btnDecode_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDecode.Click
+        lblAnswer.Text = Caesar_Cipher(txtEncode.Text, NumericUpDown1.Value, False) 'button to run the program( to shift the word to left)
+
+    End Sub
+
+    Private Sub btnEncode_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEncode.Click
+        lblAnswer.Text = Caesar_Cipher(txtEncode.Text, NumericUpDown1.Value, True)
+        'button to run the program( to shift the word to right)
+    End Sub
+End Class
+'Algorithm: 
+'First, I will get the word from user. Then, I will make the loop that can shift the words to how much the user wants E.G 2 letters shifting left or right. I will make sure if the user is using capital letter or lower case letter. Then I will show the user the value that my code got.
+
+
+'Reflection:
+'The challenges that I had is first I didn’t get the idea to shift the words. After I had the idea by searching up, I could not figure out the code for it. However, after getting advice from friend who took computer science course, I looked up in the internet and video to find code. Therefore, I was able to do the code as well. After that, I checked my code with friends so there is no mistake in it.
